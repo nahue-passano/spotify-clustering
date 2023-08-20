1b57bc255e17172a7f243ce1f7ca7a85a1bc55bd
nahue-passano <passano43632@estudiantes.untref.edu.ar>
2023-08-20 13:20:55 +0200

    update: moved make_histogram_from_dataframe to visualization.py

--- spotify_clustering/eda.py
+++ spotify_clustering/eda.py
@@ -3,8 +3,6 @@ from typing import List
 
 import pandas as pd
 import numpy as np
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
 
 
 def dataframe_stats(dataframe: pd.DataFrame, stats: List[str]) -> pd.DataFrame:
@@ -25,48 +23,6 @@ def dataframe_stats(dataframe: pd.DataFrame, stats: List[str]) -> pd.DataFrame:
     """
     df_stats = dataframe.describe(include=[np.number])
     return pd.concat([df_stats, dataframe.select_dtypes(include=np.number).agg(stats)])
-
-
-def make_histogram_from_dataframe(dataframe: pd.DataFrame) -> go.Figure:
-    """Generates a plotly figure with the histogram of each numerical feature.
-
-    Parameters
-    ----------
-    dataframe : pd.DataFrame
-        Numerical dataframe containing the data
-
-    Returns
-    -------
-    go.Figure
-        Hisogram of each dataframe feature.
-    """
-    # Init plotly figure
-    fig = make_subplots(
-        rows=4,
-        cols=4,
-        subplot_titles=dataframe.columns,
-        vertical_spacing=0.1,
-        horizontal_spacing=0.1,
-    )
-
-    for i, column_i in enumerate(dataframe.columns):
-        # Position of each trace in figure
-        row = i // 4 + 1
-        col = i % 4 + 1
-
-        trace = go.Histogram(x=dataframe[column_i], name=f"<b>{column_i}</b>")
-        fig.add_trace(trace, row=row, col=col)
-
-        # Update axes labels
-        fig.update_xaxes(title_text="Value", row=row, col=col)
-        fig.update_yaxes(title_text="Frequency", row=row, col=col)
-
-    # Layout settings
-    fig.update_layout(
-        showlegend=False, margin={"l": 0, "r": 0, "t": 30, "b": 0}, height=800
-    )
-
-    return fig
 
 
 def get_genres_from_dataframe(spotify_df: pd.DataFrame) -> List[str]:
