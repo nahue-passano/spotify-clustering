06852c9adf62d9ecad9b4589b79b53e9cee000b6
nahue-passano <passano43632@estudiantes.untref.edu.ar>
2023-08-22 01:21:12 +0200

    update: added load_yaml in utils.py

--- spotify_clustering/utils.py
+++ spotify_clustering/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import yaml
 
 import pandas as pd
 
@@ -17,3 +18,27 @@ def load_csv(csv_path: Path) -> pd.DataFrame:
         Csv data in dataframe object
     """
     return pd.read_csv(csv_path, sep=",")
+
+
+def load_yaml(yaml_path: Path) -> dict:
+    """_summary_
+
+    Parameters
+    ----------
+    yaml_path : Path
+        _description_
+
+    Returns
+    -------
+    dict
+        _description_
+    """
+
+    assert isinstance(
+        yaml_path, Path
+    ), f"YAML path {yaml_path} must be an instance of pathlib.Path"
+
+    with open(yaml_path, "r") as yaml_file:
+        yaml_dict = yaml.safe_load(yaml_file)
+
+    return yaml_dict
